2271afa4425f41e6963d0d809e83aea66fd9906d
Ibrahim Ali <69914268+Ibrahim99575@users.noreply.github.com>
2023-04-09 03:19:21 +0530

    Update urls.py

--- search_engine/urls.py
+++ search_engine/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', include('search.urls')),
+    path('', lambda request: redirect('search_results'), name='root'),
+    path('search/', lambda request: redirect('search_results'), name='root'),
     path('admin/', admin.site.urls),
 ]
+
